31ac6f992bba0f8bc32ce186e6a0896b6fffd5dc
username <your-email@email.com>
2026-04-13 15:36:11 +0800

    add

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
+  },
 };
